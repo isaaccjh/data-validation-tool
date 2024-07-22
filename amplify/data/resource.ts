d933c7f6f5822b76cad1efbec6770df425e66a33
Isaac Chan <isaac.chan@o365.activate.sg>
2024-07-22 14:58:03 +0800

    lol

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,11 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Info: a
     .model({
-      content: a.string(),
+      companyName: a.string(),
+      productName: a.string(),
+      ppgSensorModel: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
