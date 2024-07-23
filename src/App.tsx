4003e54635a44a92f868e18d3fa1d7f27e09dcd2
Isaac Chan <isaac.chan@o365.activate.sg>
2024-07-23 14:21:49 +0800

    sd

--- src/App.tsx
+++ src/App.tsx
@@ -44,19 +44,6 @@ interface LambdaResult {
   };
 }
 
-const getFormattedTimestamp = () => {
-  const now = new Date();
-  
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
-  
-  return `${year}${month}${day}${hours}${minutes}${seconds}`;
-};
-
 const App: React.FC = () => {
   const [state, setState] = useState<FormState>({
     companyName: "",
